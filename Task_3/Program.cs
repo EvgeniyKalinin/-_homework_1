a619dfb8aae253833ab31ed727f4e6fe056d84cf
lokman <kalinin5000@yandex.ru>
2022-11-28 00:21:42 +0300

    Решение третей задачи

---
+++ Task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+  if (a % 2 == 0)
+  {
+   
+  Console.Write("Вы ввели четное число");
+  }
+  else
+  {
+   Console.Write("Вы ввели нечетное число");
+  }
