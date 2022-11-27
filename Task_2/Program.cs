c9b1c9baf9c1917940389bbb7ef915a9f0f00aa3
lokman <kalinin5000@yandex.ru>
2022-11-28 00:18:12 +0300

    Решение второй задачи

---
+++ Task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите 3 числа (Через enter)");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+int min = a;
+if (b > max)
+{
+    max = b;
+}
+if (b < min) 
+{
+    min = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (c < min) 
+{
+    min = c;
+}
+Console.WriteLine("Максимальное число " + max + ".");
+Console.WriteLine($"Минимальное число {min}.");
